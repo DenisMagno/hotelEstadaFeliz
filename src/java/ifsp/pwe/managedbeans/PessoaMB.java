cdf055326735e52efe80ca192f1842cdc9f01adb
Denis Magno <denis_magno16@hotmail.com>
2018-06-25 20:33:45 -0300

    Criação das classes PessoaMB e ProprietarioMB #6

---
+++ src/java/ifsp/pwe/managedbeans/PessoaMB.java
@@ -0,0 +1,61 @@
+package ifsp.pwe.managedbeans;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public abstract class PessoaMB {
+    private Integer id;
+    private Integer cpf;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String endereco;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Integer cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
