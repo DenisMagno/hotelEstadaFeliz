86f59b738265749191bb5631b1ed2b934e8d09fb
Denis Magno <denis_magno16@hotmail.com>
2018-06-25 20:07:22 -0300

    Criação dos helpers de sessão e de filtro de login #4

--- src/java/ifsp/pwe/dao/ConnectionFactory.java
+++ src/java/ifsp/pwe/dao/ConnectionFactory.java
@@ -1,4 +1,4 @@
-package Dao;
+package ifsp.pwe.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
---
+++ src/java/ifsp/pwe/helpers/LoginFilter.java
@@ -0,0 +1,37 @@
+package ifsp.pwe.Helpers;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class LoginFilter implements Filter{
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException { }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        /*
+            TODO - Descomentar quando a bean usuario estiver pronta.
+        
+        Usuario usuario = (Usuario) ((HttpServletRequest) request).getSession().getAttribute("usuario_logado");
+
+
+        if(usuario == null){
+            ((HttpServletResponse) response).sendRedirect("index.xhtml");
+        }
+        */
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() { }   
+}
