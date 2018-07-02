5df24ecc8b81dab94a7dd15c875190e06e2151ba
Denis Magno <denis_magno16@hotmail.com>
2018-07-02 14:36:28 -0300

    Inclusão da seleção dinâmica do menu para recepcionista ou proprietário #2

--- src/java/ifsp/pwe/beans/Proprietario.java
+++ src/java/ifsp/pwe/beans/Proprietario.java
@@ -5,6 +5,16 @@ public class Proprietario extends Pessoa implements IFuncionario {
     private String senha;
     private String email;
     
+    public Proprietario(){}
+    public Proprietario(Pessoa pessoa){
+        this.id = pessoa.getId();
+        this.cpf = pessoa.getCpf();
+        this.nome = pessoa.getNome();
+        this.telefone = pessoa.telefone;
+        this.email = pessoa.email;
+        this.endereco = pessoa.endereco;
+    }
+    
     @Override
     public void obterConsumo() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
